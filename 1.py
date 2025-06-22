092c42a9ab1af0918cb56ac21cb162a511bc770e
patel vandan <137554503+patelvandan11@users.noreply.github.com>
2025-06-22 19:26:59 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -193,4 +193,5 @@ async def structured_tool(input_data: str) -> str:
 
 if __name__ == "__main__":
     print("Market Intelligence Agent server starting...")
-    mcp.run(transport="streamable-http", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=int(os.getenv("PORT", 8000)),route="/mcp")
